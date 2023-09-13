0c4516f8165b07d375163d53be734f8bc3a99d8a
st117214 <144356175+st117214@users.noreply.github.com>
2023-09-14 01:25:09 +0300

    Delete 2023.09.07-lesson-1 directory

--- 2023.09.07-lesson-1/Project4/Source.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	int K;
-	cin >> K;
-	cout << K << 9 << 9 - K;
-	return 0;
-}
--- 2023.09.07-lesson-1/Project6/Source.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	int a, b;
-	cin >> a >> b;
-	cout << (a * (a / b) + b * (b / a)) / (a / b + b / a);
-	return 0;
-}
